0696a72fc2922c6f357825e5ef6c04d4a885542d
Todd A Nordquist <toddnordquist144@gmail.com>
2019-02-21 17:11:23 -0700

    Added population count variable.

--- src/edu/cnm/deepdive/ca/model/World.java
+++ src/edu/cnm/deepdive/ca/model/World.java
@@ -1,7 +1,10 @@
 package edu.cnm.deepdive.ca.model;
 
 import java.util.Arrays;
+import java.util.BitSet;
 import java.util.Random;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
 
 public class World {
 
@@ -11,6 +14,8 @@ public class World {
   private Cell[][] next;
   private long generation;
   private final Object lock = new Object();
+  private long checksum;
+  private int population;
 
   public World() {
     this(DEFAULT_SIZE);
@@ -30,27 +35,45 @@ public class World {
       for (int col = 0; col < cellRow.length; col++) {
         if (rng.nextDouble() < threshold) {
           cellRow[col] = Cell.ALIVE;
+          population++;
         }
       }
     }
   }
 
   public void set(int row, int col, Cell cell) {
+    Cell previous = terrain[row][col];
+    if ((previous == null || previous == Cell.DEAD) && cell == Cell.ALIVE) {
+      population++;
+    } else if (previous == Cell.ALIVE && (cell == null || cell == Cell.DEAD)) {
+      population--;
+    }
     synchronized (lock) {
       terrain[row][col] = cell;
     }
   }
 
   public void tick() {
+    Checksum checksum = new CRC32();
+    BitSet bitSet = new BitSet(terrain[0].length);
+    int population = 0;
     for (int row = 0; row < terrain.length; row++) {
       for (int col = 0; col < terrain[row].length; col++) {
-        next[row][col] = terrain[row][col].next(terrain, row, col);//?
+        next[row][col] = terrain[row][col].next(terrain, row, col);
+        bitSet.set(col, next[row][col] == Cell.ALIVE);
+        if (next[row][col] == Cell.ALIVE) {
+          population++;
+        }
       }
+      byte[] rowBytes = bitSet.toByteArray();
+      checksum.update(rowBytes, 0, rowBytes.length);
     }
+    this.checksum = checksum.getValue();
     synchronized (lock) {
       for (int row = 0; row < terrain.length; row++) {
         System.arraycopy(next[row], 0, terrain[row], 0, next[row].length);
       }
+      this.population = population;
       generation++;
     }
   }
@@ -65,6 +88,14 @@ public class World {
 
   public long getGeneration() {
     return generation;
+  }
+
+  public long getChecksum() {
+    return checksum;
+  }
+
+  public int getPopulation() {
+    return population;
   }
 
   @Override
